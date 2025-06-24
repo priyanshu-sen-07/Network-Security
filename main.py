85ef24c3ba789ca8e5996665323e8f3254766a10
priyanshu-sen-07 <priyanshusen96645@gmail.com>
2025-06-24 07:57:03 +0530

    data validation completed

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 import sys
-
 
 if __name__ == "__main__":
     try:
@@ -13,7 +13,15 @@ if __name__ == "__main__":
         data_ingestion = DataIngestion(dataingestionconfig)
         logging.info('Initiate data ingestion')
         dataingestionartifact = data_ingestion.initiate_data_ingestion()
+        logging.info('data initiation completed')
         print(dataingestionartifact)
+        data_validation_config = DataValidationConfig(trainingpipelineconfig)
+        data_validation = DataValidation(dataingestionartifact,data_validation_config)
+        logging.info('initiate data validation')
+        data_validation_artifact = data_validation.initiate_data_validation()
+        logging.info('data validation completed')
+        print(data_validation_artifact)
+        
 
     except Exception as e:
         raise NetworkSecurityException(e,sys)
--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -1,7 +1,6 @@
-import os
-import pandas as pd 
+import os,sys
 import numpy as np
-import sys
+import pandas as pd
 
 """
 defining common constant variable for training pipeline
@@ -14,10 +13,28 @@ FILE_NAME: str = "phisingData.csv"
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
 
+SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
+
+SAVED_MODEL_DIR =os.path.join("saved_models")
+MODEL_FILE_NAME = "model.pkl"
+
+
+"""
+Data Ingestion related constant start with DATA_INGESTION VAR NAME
+"""
 DATA_INGESTION_COLLECTION_NAME:str = 'NetworkData'
 DATA_INGESTION_DATABASE_NAME:str = 'priyanshusen96645'
 DATA_INGESTION_DIR_NAME:str = 'data_ingestion'
 DATA_INGESTION_FEATURE_STORE_DIR:str="feature_store"
 DATA_INGESTION_INGESTED_DIR:str = 'ingested'
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATION:float=0.2
+
+"""
+data validation related constant start with DATA_VALIDATION VAR name
+"""
+DATA_VALIDATION_DIR_NAME:str='data_validation'
+DATA_VALIDATION_VALID_DIR:str="validated"
+DATA_VALIDATION_INVALID_DIR:str="invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR:str = 'dript_report'
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME:str='report.yaml'
 
